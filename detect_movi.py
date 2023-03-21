d207e9365872a4fb5377b95a33fe9729cbb93c2a
franciscofs2003 <99775859+franciscofs2003@users.noreply.github.com>
2023-03-21 08:06:21 -0600

    Update detect_movi.py

--- detect_movi.py
+++ detect_movi.py
@@ -1,44 +1,8 @@
 
 import cv2
 import numpy as np
-"""
-cap = cv2.VideoCapture('videopers.mp4')
-#sustraccion de fondo en blanco los que están en movimiento
-fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
-#kernel para mejorar la imagen binaria
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
-while True:
-    ret,img = cap.read()
-    if ret == False: break
 
-        #transforma de bgr a escala de grises
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    #Dibujamos un rectangulo en frame, para señalar el estado
-    #del área en análisis (movimiento detectado o no detectado)
-    cv2.rectangle(img,(0,0),(img.shape[1],40),(0,0,0), 1 )
-    color = (0,255,0)
-    texto_estado = "Estado: no se ha detectado movimiento"
-
-    #especificamos los puntos extremos a detectar
-    area = np.array([[240,320], [480,320], [620,img.shape[0]],[0,img.shape[0]]])
-
-    #visualizamos el alrededor del áre que vamos analizar
-    cv2.drawContours(img,[area], -1,color,2)
-
-    #visualizar el esatado de la deteccion en movimiento
-    cv2.putText(img, texto_estado, (10,30),cv2.FONT_HERSHEY_SIMPLEX,1,color,2)
-
-
-    cv2.imshow('frame', img)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    cap.release()
-    cv2.destroyAllWindows()
-"""
-
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('estacionamiento.mp4')
 fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
 kernel =  cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
 
